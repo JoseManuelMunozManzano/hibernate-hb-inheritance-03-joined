e1b032e6660efd22beb73fdcf492c6fa3e9ec415
Neimerc <Neil_Mercury@yahoo.es>
2020-05-15 16:48:36 +0200

    minor fix

--- src/main/java/com/neimerc/hibernate/demo/entity/User.java
+++ src/main/java/com/neimerc/hibernate/demo/entity/User.java
@@ -1,8 +1,6 @@
 package com.neimerc.hibernate.demo.entity;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
